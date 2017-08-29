b92c6e8d2b354d5ee337a5ac7a88cb19c3bdb374
LonelyMenStudio <30624543+LonelyMenStudio@users.noreply.github.com>
2017-08-29 19:21:45 +1000

    Tags in Spawn now hide

---
+++ Assets/OurGameStuff/Scripts/HideOnPrepEnd.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnPrepEnd : MonoBehaviour {
+
+    private bool runOnce = false;
+    private GameObject Variables;
+    private VariablesScript ManagerGet;
+    private GameObject manager;
+    private PrepPhase prepPhase;
+
+    // Use this for initialization
+    void Start () {
+        Variables = GameObject.FindWithTag("Start");
+        ManagerGet = Variables.GetComponent<VariablesScript>();
+        manager = ManagerGet.variables;
+        prepPhase = manager.GetComponent<PrepPhase>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(runOnce == true) {
+            return;
+        }
+        if (!prepPhase.inPrep) {
+            runOnce = true;
+            this.gameObject.SetActive(false);
+        }
+	}
+}
