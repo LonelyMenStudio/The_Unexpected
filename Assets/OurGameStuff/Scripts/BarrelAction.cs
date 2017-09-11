5f6334cc35cb992695f721cc534dbb39fef62d81
LonelyMenStudio <30624543+LonelyMenStudio@users.noreply.github.com>
2017-09-11 17:53:54 +1000

    just some barrel stuff

---
+++ Assets/OurGameStuff/Scripts/BarrelAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BarrelAction : NetworkBehaviour {
+
+    [SyncVar]
+    public float barrelHealth = 50;
+
+    private bool barrelDestoryed = false;
+    public GameObject childTrigger;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (barrelHealth <= 0 && !barrelDestoryed) {
+            Destroy(this.gameObject.GetComponent<MeshCollider>());
+            Destroy(this.gameObject.GetComponent<MeshRenderer>());
+            barrelDestoryed = true;
+            if (childTrigger != null) {
+                childTrigger.GetComponent<EnvCrystalHeal>().crystalHasBeenDestoryed = barrelDestoryed;
+            }
+            //Instantiate particle effect
+        }
+
+    }
+
+    public void DamageCrystal(float damage) {
+        if (!isServer) {
+            return;
+        }
+        barrelHealth = barrelHealth - damage;
+    }
+}
--- Assets/OurGameStuff/Scripts/EnvBarrelDamage.cs
+++ Assets/OurGameStuff/Scripts/EnvBarrelDamage.cs
@@ -6,6 +6,9 @@ public class EnvBarrelDamage : MonoBehaviour {
 
     public int damageAmount = 80;
     private bool sendMessage = false;
+    public bool barrelHasBeenDestoryed = false;
+    private bool damageOnce = false;
+    List<GameObject> playersInside = new List<GameObject>();
 
     // Use this for initialization
     void Start() {
@@ -14,21 +17,31 @@ public class EnvBarrelDamage : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
+        if (damageOnce) {
+            return;
+        }
+        if (barrelHasBeenDestoryed && sendMessage) {
+            for (int i = 0; i < playersInside.Count; i++) {
+                Action(playersInside[i]);
+            }
+            damageOnce = true;
+        }
     }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             sendMessage = true;
+            playersInside.Add(other.gameObject);
         }
     }
     void OnTriggerExit(Collider other) {
         if (other.gameObject.tag == "Player") {
             sendMessage = false;
+            playersInside.Remove(other.gameObject);
         }
     }
 
-    void Action(Collider other) {
+    void Action(GameObject other) {
         other.transform.SendMessage("BarrelDamage", damageAmount);
     }
 
