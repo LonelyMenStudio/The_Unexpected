958c7e9e758cf5c2faf2aaa1aef7c4340bb0c2ba
LonelyMenStudio <30624543+LonelyMenStudio@users.noreply.github.com>
2017-10-01 11:02:24 +1000

    More spawn points ready

--- Assets/OurGameStuff/Scripts/PlayerAssign.cs
+++ Assets/OurGameStuff/Scripts/PlayerAssign.cs
@@ -8,21 +8,6 @@ public class PlayerAssign : NetworkBehaviour {
     [SyncVar]
     public int playerNo = 0;
 
-    public GameObject[] weaponRespawnPoints = new GameObject[8];
-    //public GameObject[] playerRespawnPoints = new GameObject[4]; already had values
-
-	// Use this for initialization
-	void Start () {
-       // this.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);//NOPE
-
-
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-
+    public GameObject[] weaponRespawnPoints = new GameObject[28];
 
 }
