e4d8fabce2dfbd5b02820781db80ffa524831b06
Markrot <volkmann1996@hotmail.com>
2017-10-01 12:05:44 +1000

    push incase Mark broke something

---
+++ Assets/BotMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class BotMovement : MonoBehaviour {
+
+    [SerializeField]
+    Transform [] destination;
+
+    private bool Changelocation = true;
+    NavMeshAgent navMeshAgent;
+    public int i;
+	// Use this for initialization
+	void Start () {
+        i = Random.Range(0, destination.Length);
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (navMeshAgent == null) {
+            Debug.LogError("The nav mesh agent compnent is not attached to" + gameObject.name);
+        }else {
+            SetDestination();
+        }
+	}
+	
+    private void SetDestination() {
+       
+        if ( destination != null) {
+            if (Changelocation == true) {
+                i = Random.Range(0, destination.Length);
+                Changelocation = false;
+            }
+
+            Vector3 tragetVector = destination[i].transform.position;
+            navMeshAgent.SetDestination(tragetVector);
+        }
+    }
+}
