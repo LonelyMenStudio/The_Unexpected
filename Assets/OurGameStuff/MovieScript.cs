e252873b26b840c8c40baed4665214076ed0a2fa
Markrot <volkmann1996@hotmail.com>
2017-10-08 02:08:36 +1000

    I hate unity...

---
+++ Assets/OurGameStuff/MovieScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class MovieScript : MonoBehaviour {
+
+
+    public MovieTexture movie;
+    private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape) && movie.isPlaying) {
+            movie.Stop();
+        }
+	}
+}
