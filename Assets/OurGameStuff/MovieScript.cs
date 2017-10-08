eadec64d46800e19c004a3bb629f11ce39b57960
Markrot <volkmann1996@hotmail.com>
2017-10-08 16:47:20 +1000

    Scene at the start, just need to drop the vid in

--- Assets/OurGameStuff/MovieScript.cs
+++ Assets/OurGameStuff/MovieScript.cs
@@ -1,28 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
-[RequireComponent (typeof(AudioSource))]
+//[RequireComponent (typeof(AudioSource))]
 
 public class MovieScript : MonoBehaviour {
 
+    /*  public RawImage image;
+     // public MovieTexture movie;
+     private VideoPlayer videoPlayer;
+      public VideoClip Vid;
+      private AudioSource audiosource;
+      private VideoSource videoSource;
+      // Use this for initialization
+      void Start () {
+          videoPlayer.source = VideoSource.VideoClip;
+          videoPlayer.clip = Vid;
+      }
 
-    public MovieTexture movie;
-    private AudioSource audio;
+      // Update is called once per frame
+      void Update () {
+          if (Input.GetKey(KeyCode.Escape)) {
 
-	// Use this for initialization
-	void Start () {
-        GetComponent<RawImage>().texture = movie as MovieTexture;
-        audio.clip = movie.audioClip;
-        movie.Play();
-        audio.Play();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.Escape) && movie.isPlaying) {
-            movie.Stop();
+          }
+      }*/
+    public GameObject canvi;
+    public VideoPlayer video;
+    void Start() {
+        video.Play();
+        canvi.SetActive(false);
+    }
+    void Update() {
+        if (Input.GetKey(KeyCode.Escape)) {
+            canvi.SetActive(true);
+            video.Stop();
         }
-	}
+    }
 }
