5a6aed3ca1fd99a66495d7eb753c72d38b5e565d
LonelyMenStudio <30624543+LonelyMenStudio@users.noreply.github.com>
2017-10-14 16:24:11 +1000

    Can manual connect

---
+++ Assets/OurGameStuff/Scripts/LobbyManual.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManual : MonoBehaviour {
+
+    public GameObject button;
+    public GameObject title;
+    public GameObject create;
+    public GameObject input;
+    public GameObject join;
+
+    public void displayManual() {
+        title.SetActive(true);
+        create.SetActive(true);
+        input.SetActive(true);
+        join.SetActive(true);
+        button.SetActive(false);
+    }
+}
