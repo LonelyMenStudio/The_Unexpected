bad6fd1b6ec1e0ea323bb069d34a478188d4e143
LonelyMenStudio <30624543+LonelyMenStudio@users.noreply.github.com>
2017-10-17 12:24:45 +1000

    MOUSE :mouse: PROTECTOR :oncoming_police_car:

---
+++ Assets/OurGameStuff/Scripts/MouseProtector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseProtector : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
