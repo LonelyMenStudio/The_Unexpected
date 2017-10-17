92d2341a3b2124ee0684c031816ec96372c95034
LonelyMenStudio <30624543+LonelyMenStudio@users.noreply.github.com>
2017-10-17 19:03:27 +1000

    Res Settings :partly_sunny:

---
+++ Assets/OurGameStuff/Scripts/GraphicsSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsSet : MonoBehaviour {
+
+    private int inputX;
+    private int inputY;
+    private bool fullscreen = true;
+
+    public GameObject xGet;
+    public GameObject yGet;
+
+    public void setGraphics() {
+        if (xGet.GetComponent<InputField>().textComponent.text != null && yGet.GetComponent<InputField>().textComponent.text != null) {
+            inputX = int.Parse(xGet.GetComponent<InputField>().textComponent.text);
+            inputY = int.Parse(yGet.GetComponent<InputField>().textComponent.text);
+        }
+        Screen.SetResolution(inputX, inputY, fullscreen);
+    }
+}
