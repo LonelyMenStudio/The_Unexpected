c1d1ee5f343cef016a5dcb69e6515585dccb32c1
LonelyMenStudio <30624543+LonelyMenStudio@users.noreply.github.com>
2017-10-24 16:52:55 +1000

    Credit Screen Start

--- Assets/OurGameStuff/Scripts/SceneLoader.cs
+++ Assets/OurGameStuff/Scripts/SceneLoader.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using GameAnalyticsSDK;
 
-
 public class SceneLoader : MonoBehaviour {
-    /* public bool makeitwork = false;
-     void Start() {
-         GameObject[] waypointArray;
-         waypointArray = GameObject.FindGameObjectsWithTag("Network");
-         if ( waypointArray.Length != 0) {
-             makeitwork = true;
-         }
-     }*/
     public void loadscene(int sceneIndex) {
         if (sceneIndex == 1) {
             GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "Going to Lobby");
@@ -21,12 +12,6 @@ public class SceneLoader : MonoBehaviour {
         else if (sceneIndex == 0) {
             GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "Going to Menu");
         }
-        // if (makeitwork) {
-        //     GameObject Lobby = GameObject.FindWithTag("NetWork");
-        //     Lobby.SetActive(true);
-        //       SceneManager.LoadScene(sceneIndex);
-        //  } else {
         SceneManager.LoadScene(sceneIndex);
-        //    }
     }
 }
